a617e6dbdacb9a9e44d82caa386af74d7b8d9c84
Wim Muskee <wimmuskee@gmail.com>
2016-12-23 12:57:18 +0100

    move some stuff to create functions

--- share/create.sh
+++ share/create.sh
@@ -1,7 +1,18 @@
 # This file is part of the cleanstart package.
 
+function setMisc {
+	local basedir=$1
+	cp -L /etc/resolv.conf ${basedir}/etc/
+	mkdir -p ${basedir}/usr/portage
+}
+
+function setBashrc {
+	local basedir=$1
+	echo "PS1='\[\033]0;\u@\h:\w\007\]\[\033[01;31m\]\h\[\033[01;34m\] \W \$ \[\033[01;33m\][chroot] \[\033[00m\]'" >> ${basedir}/root/.bashrc
+}
+
 # create new profile config file if not exists
-function createProfileFile {
+function setProfileFile {
 	local profilefile=$1
 
 	if [[ ! -f ${profilefile} ]]; then
